ab0b5cc50c625a9130dfc143366cfaa9d8e25146
hradaysadrani <hradaysadrani@gmail.com>
2023-08-26 19:35:20 +0530

    Update pdfmerger.py
    Added an alternate code where pdf will be merged according to the sequence same as in the folder.

--- PDFMerger/pdfmerger.py
+++ PDFMerger/pdfmerger.py
@@ -1,9 +1,24 @@
-from pypdf import PdfWriter
+# from pypdf import PdfWriter
 
-merger = PdfWriter()
+# merger = PdfWriter()
 
-for pdf in ["\Hraday\Python tutorial\Python tutorial projects\PDFMerger\example1.pdf", "\Hraday\Python tutorial\Python tutorial projects\PDFMerger\example2.pdf", "\Hraday\Python tutorial\Python tutorial projects\PDFMerger\example3.pdf"]:
-    merger.append(pdf)
+# for pdf in ["\Hraday\Python tutorial\Python tutorial projects\PDFMerger\example1.pdf", "\Hraday\Python tutorial\Python tutorial projects\PDFMerger\example2.pdf", "\Hraday\Python tutorial\Python tutorial projects\PDFMerger\example3.pdf"]:
+#     merger.append(pdf)
 
-merger.write("merged-pdf.pdf")
-merger.close()
+# merger.write("merged-pdf.pdf")
+# merger.close()
+
+# The below code will merge pdf in a sequence same as the folder itself.
+
+import os
+from PyPDF2 import PdfMerger
+
+x = [a for a in os.listdir() if a.endswith(".pdf")]
+
+merger = PdfMerger()
+
+for pdf in x:
+    merger.append(open(pdf, 'rb'))
+
+with open("result.pdf", "wb") as fout:
+    merger.write(fout)
